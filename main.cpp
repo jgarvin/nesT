60ae7b3a70dd518ab90210ac7ab1fbaa4001bc99
Joseph <prophet@citadel.(none)>
2009-08-27 22:30:58 -0500

    Got Boost working!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <string>
 
 #include <QApplication>
 #include <QPushButton>
 
+#include <boost/filesystem/fstream.hpp>
+
+// We'll use toast::logger, this is just to make sure boost is linking right
+using namespace boost::filesystem;
+class Log {
+public:
+	Log(std::string const & filename)
+		: path_(filename),
+		  logstream_(path_)
+	{
+	}
+
+	~Log()
+	{
+		logstream_.close();
+	}
+	
+	void writeToLog(std::string const & toLog)
+	{
+		logstream_ << toLog << std::endl;
+	}
+
+private:
+	path path_;
+	ofstream logstream_;
+};
+
 int main(int argc, char *argv[])
 {
+	Log log("nesTLog.txt");
+	log.writeToLog("nesT starting up...");
+	
 	QApplication app(argc, argv);
 
 	QPushButton hello("Hello world!");
