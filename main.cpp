06855211e57439f1d3ce4fbe92ff5f1c622bf58b
Joseph <prophet@citadel.(none)>
2009-08-27 21:49:41 -0500

    Turn into QT4 hello world app

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "nesT starting up..." << std::endl;
+#include <QApplication>
+#include <QPushButton>
 
-	
-	
-	return 0;
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	QPushButton hello("Hello world!");
+	hello.resize(100, 30);
+
+	hello.show();
+	return app.exec();
 }
