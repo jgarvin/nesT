9e534ed7952cdc32b79f54cc83cd1bd8e43f9568
Joseph <prophet@citadel.(none)>
2009-09-13 22:40:46 -0500

    Add command line option for picking a ROM to load, and code that starts reading the file. All it does is check the first 3 bytes are 'NES' and that says success. A bit premature I know ;)

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <stdexcept>
 
 #include <QApplication>
-//#include <QPushButton>
 
-#include <boost/filesystem/fstream.hpp>
+#include <boost/program_options.hpp>
 
 #include <SDL/SDL.h>
 
 #include "main_window.hpp"
+#include "rom.hpp"
+
+namespace po = boost::program_options;
 
 void SDLTest()
 {
@@ -17,12 +21,11 @@ void SDLTest()
 }
 
 // We'll use toast::logger, this is just to make sure boost is linking right
-using namespace boost::filesystem;
 class Log {
 public:
 	Log(std::string const & filename)
 		: path_(filename),
-		  logstream_(path_)
+		  logstream_(filename.c_str())
 	{
 	}
 
@@ -37,14 +40,47 @@ public:
 	}
 
 private:
-	path path_;
-	ofstream logstream_;
+	std::string path_;
+	std::ofstream logstream_;
 };
 
 int main(int argc, char *argv[])
 {
 	Log log("nesTLog.txt");
 	log.writeToLog("nesT starting up...");
+
+    try {
+        po::options_description desc("nesT allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("rom", po::value<std::string>(), "NES rom to load")
+        ;
+
+        po::variables_map vm;        
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);    
+
+        if (vm.count("help")) {
+			std::cout << desc << "\n";
+            return 1;
+        }
+
+        if (vm.count("rom")) {
+			std::cout << "Loading rom: " 
+					  << vm["rom"].as<std::string>() << ".\n";
+
+			std::ifstream rom_file(vm["rom"].as<std::string>().c_str(), std::ios::binary);
+			if(!rom_file.is_open())
+				throw std::runtime_error("Couldn't open specified ROM file. Make sure the file exists and you have proper permissions.");
+			Rom cmd_line_rom(rom_file);
+        } else {
+			std::cout << "No ROM selected.\n";
+        }
+    }
+    catch(std::exception& e) {
+		std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
 
 	log.writeToLog("testing SDL....");
 	SDLTest();
