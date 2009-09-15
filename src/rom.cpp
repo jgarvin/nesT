2603897b1577692eb43dffdb61c4f0719c134d9f
Joseph <prophet@citadel.(none)>
2009-09-14 19:55:50 -0500

    Add initial code for figuring out the number of prg, chr banks and title

--- src/rom.cpp
+++ src/rom.cpp
@@ -1,7 +1,9 @@
-#include <fstream>
 #include <stdexcept>
 #include <cstring>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
 
 #include "rom.hpp"
 
@@ -9,12 +11,13 @@ Rom::Rom(std::ifstream& file)
 {
 	file.seekg(0, std::ios::end);
 	std::streampos size = file.tellg();
-    char* raw_nes_data = new char [size];
+    raw_nes_data_.reserve(size);
 
 	std::cout << "File size: " << size << std::endl;
 
     file.seekg(std::ios::beg);
-    file.read(raw_nes_data, size);
+	std::copy(std::istream_iterator<char>(file), std::istream_iterator<char>(),
+			  std::back_inserter(raw_nes_data_));
     file.close();
 
 	// Read ROM according to docs at:
@@ -22,7 +25,7 @@ Rom::Rom(std::ifstream& file)
 
 	char header[4];
 	for(int i = 0; i < 3; ++i)
-		header[i] = raw_nes_data[i];
+		header[i] = raw_nes_data_[i];
 	header[3] = '\0';
 
 	if(std::strcmp(header, "NES") != 0) {
@@ -31,4 +34,35 @@ Rom::Rom(std::ifstream& file)
 	}
 
 	std::cout << "Rom load success!" << std::endl;
+
+	std::cout << toString();
+}
+
+int Rom::num_prg_banks() const
+{
+	// 4 is constant offset determined by .NES format
+	return raw_nes_data_[4];
+}
+
+int Rom::num_chr_banks() const
+{
+	// 5 is constant offset determined by .NES format
+	return raw_nes_data_[5];
+}
+
+std::string Rom::title() const
+{
+	const char* title_start = &(*(raw_nes_data_.end() - 122));
+	return std::string(title_start);
+}
+
+std::string Rom::toString() const
+{
+	std::ostringstream o;
+
+	o << "PRG Banks: " << num_prg_banks() << std::endl
+	  << "CHR Banks: " << num_chr_banks() << std::endl
+	  << "Title: " << title() << std::endl;
+
+	return o.str();
 }
--- src/rom.hpp
+++ src/rom.hpp
@@ -2,11 +2,22 @@
 #define INCLUDED_NEST_ROM_HPP
 
 #include <fstream>
+#include <string>
+#include <vector>
 
 class Rom
 {
 public:
 	Rom(std::ifstream& file);
+
+	int num_prg_banks() const;
+	int num_chr_banks() const;
+	std::string title() const;
+
+	std::string toString() const;
+
+private:
+	std::vector<char> raw_nes_data_;
 };
 
 #endif
